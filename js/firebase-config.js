2fb3eec074ba7869a302e8e95b9afa5f9f2223c8
Sapi <ferreteriasapi@gmail.com>
2026-05-30 00:23:16 -0500

    Fix Firebase API key and credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,12 +5,12 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 
 // Configuración de Firebase de STORE SAPI provista por el usuario
 const firebaseConfig = {
-  apiKey: "AIzaSyAnmmSoayKSbWhPlq3l1kFyabZI9xxXJUs",
+  apiKey: "AIzaSyAnmmSoayKSbWhPlq3l1kFyabZI9xXxJUs",
   authDomain: "store-sapi.firebaseapp.com",
   projectId: "store-sapi",
   storageBucket: "store-sapi.firebasestorage.app",
-  messagingSenderId: "47423909272",
-  appId: "1:47423909272:web:c14fc1a4b502806fab5a3d",
+  messagingSenderId: "474239099272",
+  appId: "1:474239099272:web:c14fc1a4b502806fab5a3d",
   measurementId: "G-35RL4SQ4HG"
 };
 
